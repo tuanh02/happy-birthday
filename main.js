88af4324c8fee9f65c7f45c1640bcd3b8ce68f5a
tuanh02 <160607084+tuanh02@users.noreply.github.com>
2024-02-20 21:06:11 +0700

    Happy Birthday

---
+++ main.js
@@ -0,0 +1,21 @@
+function createBur(){
+    const animationContainer = document.querySelector('.animation-container')
+    const burst = document.querySelector('.burst')
+    const lines = document.querySelector('.line')
+    burst.style.top = Math.random() * innerHeight + 'px'
+    burst.style.left = Math.random() * innerWidth + 'px'
+
+    const color = ["#ea5e55","#34a854","#4285f4","#fbbc05","#fdfdfd","ocean"]
+    lines.foreach(line =>{
+        const randomColor = color[Math.floor(Math.random()*color.length)]
+        line.style.background = randomColor
+        
+    })
+    const burstClone = burst.cloneNode(true);
+    animationContainer.appendChild(burstClone)
+    setTimeout(()=>{
+        burstClone.remove();
+    },3000)
+}
+setInterval(createBur, 500)
+
